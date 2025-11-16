475c01b16f28bea6bd379469d05e1ca1594ddfcc
Giorgos Seimenis <>
2025-11-16 15:07:54 +0200

    removed unused imports in login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,12 @@
 import { Container, Row, Col, Card, Form, Button, Spinner, InputGroup } from 'react-bootstrap';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import TokenHelper from '../services/TokenHelper';
-import RequestHelper from "../services/RequestHelper.ts";
 import { useCarStore } from '../services/managers/CarManager.ts';
 import {toast, ToastContainer} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useFuelFillRecordStore } from '../services/managers/FuelFillRecordManager.ts';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
-import { faGasPump } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from "react-router";
 import { useMalfunctionStore } from '../services/managers/MalfunctionManager.ts';
 import { useServiceStore } from '../services/managers/ServiceManager.ts';
